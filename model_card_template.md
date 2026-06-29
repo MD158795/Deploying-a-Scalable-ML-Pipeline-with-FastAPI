287263e37e3c4f0eeb87cb2dfa1ad8f2f1829449
Michael Diehl <mdieh16@wgu.edu>
2026-06-29 18:19:46 +0000

    Completed Part 4: Train Model.py and Model Card

--- model_card_template.md
+++ model_card_template.md
@@ -3,16 +3,23 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
-
+This model is a supervised binary classification model trained to predit whether an individual's income would be greater than 50k, or less than or equal to 50k. The model uses a RandomForestClassifier from scikit-learn. Categorical features are processed using a one-hot encoding method. The target label was processed using a labal binarizer. 
 ## Intended Use
-
+This model is intended to be used as a demonstration of end to end machine learning for the Udacity MLDevOps nanodegree. It is for educational purposes only. 
 ## Training Data
-
+The model was trained using the census.csv database. The target variable was 'salary', which indicates whether an individuals income was less than or equal or greater than 50k. 
+The data was split into 80% training and 20% test data using a random seed of 42. The training data was used to fit the encoder, label binarizer, and random forest. 
+The categorical features used in preprocessing were "workclass","education," "marital status", occupation", relationship", "race","sex", and "native country".
 ## Evaluation Data
-
+The evaluation data came from 20% of the test split from the database. The model was evaluated on the full test dataset and on categorical slices of the test dataset. Slice was performace was computed for each unique value in every categorical feature. 
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
+The model was evaluated on using precision,recall, and F1 score. The model achieved the following results:
+Precision: .7419
+Recall: .6384
+F1 Score: .6863
 
+The model was also evaluated on categorical slices. These are included in the slice_output.txt file. 
 ## Ethical Considerations
-
+This dataset contains demographic and socioeconomic features. The model's slice performance varies across races, sex, and native country. Additional bias analyis should be considered before adopting this model for real-world decision making. 
 ## Caveats and Recommendations
+This model was developed for educational purposes only. 
--- train_model.py
+++ train_model.py
@@ -13,14 +13,24 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "Your path here"
+project_path = os.getcwd()
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = None # your code here
+data = pd.read_csv(data_path)
+
+# Clean column names
+data.columns = data.columns.str.strip()
+# Clean string values with leading/trailing spaces
+for col in data.select_dtypes(include=["object"]).columns:
+    data[col] = data[col].str.strip()
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = None, None# Your code here
+train, test = train_test_split(
+    data,
+    test_size=0.20,
+    random_state=42,
+)
 
 # DO NOT MODIFY
 cat_features = [
@@ -36,10 +46,10 @@ cat_features = [
 
 # TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
-    # your code here
-    # use the train dataset 
-    # use training=True
-    # do not need to pass encoder and lb as input
+    train,
+    categorical_features = cat_features,
+    label="salary",
+    training = True,
     )
 
 X_test, y_test, _, _ = process_data(
@@ -52,7 +62,7 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = None # your code here
+model = train_model(X_train, y_train)
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
@@ -66,7 +76,8 @@ model = load_model(
 ) 
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = None # your code here
+preds = inference(model, X_test)
+
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
@@ -79,8 +90,14 @@ for col in cat_features:
     for slicevalue in sorted(test[col].unique()):
         count = test[test[col] == slicevalue].shape[0]
         p, r, fb = performance_on_categorical_slice(
-            # your code here
-            # use test, col and slicevalue as part of the input
+            data=test,
+            column_name=col,
+            slice_value=slicevalue,
+            categorical_features=cat_features,
+            label="salary",
+            encoder=encoder,
+            lb=lb,
+            model=model,
         )
         with open("slice_output.txt", "a") as f:
             print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
